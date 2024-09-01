a9790251606c0d7e1039071875fc1834b9732b25
Adonis <tafrikhan.rizki.praditya@students.untidar.ac.id>
2024-09-01 10:08:46 +0700

    Menambahkan Proyek1

---
+++ Proyek1/2320506018_Tafrikhan_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tafrikhan_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Tafrikhan_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama    : Tafrikhan");
+        System.out.println("NPM     : 2320506018");
+        System.out.println("Alamat  : Magelang");
+        System.out.println("No_HP   : 08123456778");
+    }
+}
